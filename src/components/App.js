0fa1866bc56fc244be190090a694c864462ee082
David Dionise <david.dionise@gmail.com>
2018-06-25 11:26:07 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const App = () => {
+  return (
+    <div className='app-continer'>
+      HELLO WOOOOORLD
+    </div>
+  )
+}
+
+export default App;
